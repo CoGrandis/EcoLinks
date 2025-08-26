7e65367c41be189af1f6c061d10cad3efbfcabaa
CoGrandis <grandisconstanza@gmail.com>
2025-08-26 20:06:17 -0300

    Controllers

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+class HomeController {
+    public function index() {
+        require __DIR__ . '/../views/home.php';
+    }
+    public function about() {
+        require __DIR__ . '/../views/about.php';
+    }
+}
+?>
