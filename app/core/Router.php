bb8ddc5fce26500af7149d054ff7efeb1e5b8204
CoGrandis <grandisconstanza@gmail.com>
2025-09-02 18:55:07 -0300

    backend: desarrollo la clase router y la implemento en index-php

---
+++ app/core/Router.php
@@ -0,0 +1,42 @@
+<?php
+class Router
+{
+    protected static array $routes = [];
+
+    public static function get($route, $controller, $action): void
+    {
+        self::$routes['get'][strtolower($route)] = [$controller, $action];
+    }
+
+    public function resolve($requestUri, $requestMethod): void
+    {
+        $path = parse_url($requestUri, component: PHP_URL_PATH);
+        $method = strtolower($requestMethod);
+
+        if (isset(self::$routes[$method][$path])) {
+            [$controller, $action] = self::$routes[$method][$path];
+            $this->callAction($controller, $action);
+            return;
+        }
+
+        echo "404 - Page not found!";
+    }
+
+    private function callAction($controller, $action): void
+    {
+        $controllerClass = "App\\controllers\\{$controller}";
+
+        if (class_exists($controllerClass)) {
+            $controllerInstance = new $controllerClass();
+
+            if (method_exists($controllerInstance, $action)) {
+                $controllerInstance->$action();
+                return;
+            }
+        }
+
+        echo "404 - Controller or action not found!";
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
-$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$request = trim($request, '/');
+require_once 'core/Router.php';
 
-$segments = explode('/', $request);
+$route =  new Router();
 
-$controllerName = !empty($segments[0]) ? ucfirst($segments[0]) . 'Controller' : 'HomeController';
-$methodName = $segments[1] ?? 'index';
+$route->get('/','HomeController', 'index' );
 
-$controllerFile = __DIR__ . "/app/controllers/{$controllerName}.php";
-
-if (file_exists($controllerFile)) {
-    require_once $controllerFile;
-    $controller = new $controllerName();
-
-    if (method_exists($controller, $methodName)) {
-        $controller->$methodName();
-    } else {
-        http_response_code(404);
-        echo "404 - Método no encontrado";
-    }
-} else {
-    http_response_code(404);
-    echo "404 - Controlador no encontrado";
-}
+$route->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);   
+?>
