57acc7fa9ea360ab5811acbcd4cebd71bd4b33ce
CoGrandis <grandisconstanza@gmail.com>
2025-09-29 19:34:37 -0300

    frontend: pantalla de registro de empleados

--- app/controllers/EmployeeController.php
+++ app/controllers/EmployeeController.php
@@ -2,7 +2,9 @@
 
 class EmployeeController {
     public function register() {
+        $current_page = basename($_SERVER['REQUEST_URI']);
         $tpl = new TemplateMotor("employee-form");
+        $tpl->assing(["EMPLOYEES_ACTIVE" => (strpos($current_page, 'employee') !== false) ? 'active' : '']);
         $tpl->printToScreen();
     }
 }
