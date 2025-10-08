c2e9ac9bed8b0c20d63cdcfd47dbede7d89f79e6
CoGrandis <grandisconstanza@gmail.com>
2025-10-07 21:25:32 -0300

    feat: Implementacion de PostModel ;actualiza views employee list y profile

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -1,3 +1,21 @@
 <?php
-    c
+
+class PostController
+{
+    public function create()
+    {
+        $tpl = new TemplateMotor("create-post");
+        $tpl->printToScreen();
+    }
+
+    public function list()
+    {
+        $tpl = new TemplateMotor("list");
+        $tpl->printToScreen();
+    }
+
+    public function delete($id)
+    {
+    }
+}
 ?>
