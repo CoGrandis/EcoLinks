874cab773c7b28bdbabfe33573012ad71285a6e5
eliana-001 <alonherrera02@gmail.com>
2025-10-17 00:28:51 -0300

    se edito perfil para que sea compatible con la edicion de datos en la cual pueda subir, su firma. su cv y numeros de contactos

--- app/views/extends/MenuExtends.php
+++ app/views/extends/MenuExtends.php
@@ -1,6 +1,10 @@
+
 <nav class="admin-menu">
+   
     <ul>
+        <img src="../../../assets/img/logo/logo.svg" alt="">
         <li class="{{ PROFILE_ACTIVE }}"><i class=" icon fa-solid fa-user"></i><a href="{{ APP_URL }}/profile/{{ EMPLOYEE }}">Perfil</a></li>
+        <li class=""><i class="fa fa-calendar" aria-hidden="true"></i> Calendario</li>
         <li class="{{ FILES_ACTIVE }}"><i class=" icon fa-solid fa-file"></i><a href="{{ APP_URL }}/files">Documentos</a></li>
         <li class="{{ NEWS_ACTIVE }}"><i class=" icon fa-solid fa-feather-pointed"></i><a href="{{ APP_URL }}/news">Muro Informativo</a></li>
         <li><i class=" icon fa-solid fa-left-from-bracket"></i><a href="{{ APP_URL }}auth/logout">Cerrar Sesión</a></li>
