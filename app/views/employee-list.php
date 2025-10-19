5be7cdd9987fdade1ba61e21c6b9c2eb6c65c7e7
eliana-001 <alonherrera02@gmail.com>
2025-10-19 18:16:26 -0300

    feat: add views for Reclamos and ListaReclamos with respective styles and functionality

---
+++ app/views/employee-list.php
@@ -0,0 +1,26 @@
+@extends(head)
+<link rel="stylesheet" href="../../assets/css/emple.css">
+<body>
+    <section class="admin-dashboard">
+        @extends(menu)
+        
+        <main class="main-dashboard"> 
+            <h1>Lista de Empleados</h1>
+            <div class=".emple-searchbar">
+                <form action="" method="post">
+                <input type="text" name="search"placeholder="Buscar empleado...">
+                <input type="submit" value="">
+            </form>
+                
+            </div>
+           
+            
+            <section class="employee-list">
+                    
+            <?php foreach ($employees as $employee) : ?>
+                <?= $employee["Nombre"] ?>
+            <?php endforeach;?>
+            </section>
+    </section>
+</body>
+</html>
